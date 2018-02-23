0adc82c2953dc701c22a2e76d1e1003d56b45188
John.Xu <john.xu89@gmail.com>
2018-02-23 16:19:05 -0600

    add routes and controllers

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var Task = require('./api/models/todoListModel');
+var bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
 
 app.listen(port);
 
